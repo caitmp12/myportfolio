5aa4acc2222133e40b5e39e221746429ca3eb6a0
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-01-15 15:34:18 -0500

    created a skills section in about page

--- components/about.js
+++ components/about.js
@@ -1,11 +1,19 @@
 const About = () => {
     return (
+        <>
         <div className="about-container">
             <div className="about">
             <h2>A little about me...</h2>
             <p>I am a full stack developer with a background in media sales and creative writing. My career in sales has given me invaluable experience executing the demands of numerous clients while simultaneously meeting management’s expectations. My talent as a writer helps me come at problems from a more creative angle and, combined with my hard skills, allows me to emphasize aesthetic as well as functionality.</p>
             </div>
         </div>
+        <div className="skills-section">
+            <h2>Languages</h2>
+            <div className="skills-container">
+                <div>JavaScript</div>
+            </div>
+        </div>
+        </>
   )
 }
 

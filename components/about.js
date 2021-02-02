82f5fae0fa8170a25272a6a4480c84d2e81c9673
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-02-01 19:03:36 -0500

    commented out content

--- components/about.js
+++ components/about.js
@@ -7,12 +7,12 @@ const About = () => {
             <p>I am a full stack developer with a background in media sales and creative writing. My career in sales has given me invaluable experience executing the demands of numerous clients while simultaneously meeting management’s expectations. My talent as a writer helps me come at problems from a more creative angle and, combined with my hard skills, allows me to emphasize aesthetic as well as functionality.</p>
             </div>
         </div>
-        <div className="skills-section">
+        {/* <div className="skills-section">
             <h2>Languages</h2>
             <div className="skills-container">
                 <div>JavaScript</div>
             </div>
-        </div>
+        </div> */}
         </>
   )
 }
